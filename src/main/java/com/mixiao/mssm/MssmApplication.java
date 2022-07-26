9e5b7f49cdf298533f2f98345010bc3ea6f20b7c
Mixiaoli <2324598590@qq.com>
2022-07-26 14:47:38 +0800

    创建基本Mssm项目文件

---
+++ src/main/java/com/mixiao/mssm/MssmApplication.java
@@ -0,0 +1,13 @@
+package com.mixiao.mssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MssmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mixiao/mssm/MssmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixiao.mssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
